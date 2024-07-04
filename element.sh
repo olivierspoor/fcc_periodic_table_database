2ea025bfd4f60cdb69e40131e987083758cbc48e
Olivier Spoor <o.spoor@proton.me>
2024-07-04 13:31:21 +0000

    feat: check if argument in atomic_number

--- element.sh
+++ element.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ -z $1 ]]
   then
   echo Please provide an element as an argument.
   exit 2
 fi
+
+ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements")
+
+for x in $ATOMIC_NUMBER; do
+  if [[ $x = $1 ]]; then
+    ATOMIC_NUMBER=$1
+  fi
+done
+
+echo $ATOMIC_NUMBER
+
