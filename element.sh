e827ab484951f67ccfca977b93748aaa096b29cd
Olivier Spoor <o.spoor@proton.me>
2024-07-04 14:06:05 +0000

    feat: check argument and go to function

--- element.sh
+++ element.sh
@@ -8,13 +8,52 @@ if [[ -z $1 ]]
   exit 2
 fi
 
-ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements")
+GOT_ATOMIC_NUMBER() {
+  echo $ATOMIC_NUMBER
+  exit 0
+}
 
-for x in $ATOMIC_NUMBER; do
+GOT_SYMBOL() {
+  echo $SYMBOL
+  exit 0
+}
+
+GOT_NAME() {
+  echo $NAME
+  exit 0
+}
+
+ATOMIC_NUMBER_LIST=$($PSQL "SELECT atomic_number FROM elements")
+
+for x in $ATOMIC_NUMBER_LIST; do
   if [[ $x = $1 ]]; then
     ATOMIC_NUMBER=$1
+    GOT_ATOMIC_NUMBER
   fi
 done
 
-echo $ATOMIC_NUMBER
+if [[ -z $ATOMIC_NUMBER ]]
+  then
+  SYMBOL_LIST=$($PSQL "SELECT symbol FROM elements")
 
+  for x in $SYMBOL_LIST; do
+    if [[ $x = $1 ]]; then
+      SYMBOL=$1
+      GOT_SYMBOL
+    fi
+  done
+fi
+
+if [[ -z $SYMBOL ]]
+  then
+  NAME_LIST=$($PSQL "SELECT name FROM elements")
+
+  for x in $NAME_LIST; do
+    if [[ $x = $1 ]]; then
+      NAME=$1
+      GOT_NAME
+    fi
+  done
+fi
+
+echo "I could not find that element in the database."
