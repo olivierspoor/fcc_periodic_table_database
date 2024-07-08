f7c0450b68f92b5aaffcaf38352f70f87b27e13c
Olivier Spoor <o.spoor@proton.me>
2024-07-08 07:18:21 +0000

    feat: output according to task

--- element.sh
+++ element.sh
@@ -9,23 +9,31 @@ if [[ -z $1 ]]
 fi
 
 GOT_ATOMIC_NUMBER() {
-  echo $ATOMIC_NUMBER
   ATOMIC_RESULT=$($PSQL "SELECT name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius  FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER")
-  echo $ATOMIC_RESULT
+  echo $ATOMIC_RESULT | while IFS="|" read NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+  do
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
   exit 0
 }
 
 GOT_SYMBOL() {
   echo $SYMBOL
   SYMBOL_RESULT=$($PSQL "SELECT atomic_number, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius  FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE symbol='$SYMBOL'")
-  echo $SYMBOL_RESULT
+  echo $SYMBOL_RESULT | while IFS="|" read ATOMIC_NUMBER NAME TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+  do
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
   exit 0
 }
 
 GOT_NAME() {
   echo $NAME
   NAME_RESULT=$($PSQL "SELECT atomic_number, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius  FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE name='$NAME'")
-  echo $NAME_RESULT
+  echo $NAME_RESULT | while IFS="|" read ATOMIC_NUMBER SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+  do
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
   exit 0
 }
 
